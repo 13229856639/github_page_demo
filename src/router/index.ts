8b803bf924e2141e1fb8baad6839aee59c5efdcc
13229856639 <1434789127@qq.com>
2022-11-19 15:24:08 +0800

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,7 +19,8 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory("github_page_demo"),
   routes,
 });
 
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  publicPath: "/github_page_demo/",
   transpileDependencies: true,
 });
